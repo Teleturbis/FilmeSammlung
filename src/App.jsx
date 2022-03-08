e203b153793ab639b47b515dc910f3f4d958fab8
Christian Löffler <cloeffle74@gmail.com>
2022-03-08 17:58:14 +0100

    Imported plugins, contentFul, uuid

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Routes, Route, NavLink, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import * as Contentful from 'contentful';
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
