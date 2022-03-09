3c98a7c49b393a1158a18cf1b039cb7f39c6bba1
Christian Löffler <cloeffle74@gmail.com>
2022-03-09 18:07:41 +0100

    implementing Genres

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   }, []);
 
   async function fetchData() {
-    let fetching = await client.getEntries();
+    let fetching = await client.getEntries({ content_type: 'film' });
     setFilms(fetching.items);
   }
 
@@ -57,8 +57,14 @@ function App() {
   return (
     <div>
       <Header />
-      <Caroussel randomFilms={randomFilms} />
       <div>{films && console.log(films)}</div>
+      <Routes>
+        <Route
+          path="/genre"
+          element={<Genres client={client} films={films} />}
+        />
+        <Route path="/" element={<Caroussel randomFilms={randomFilms} />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/Genres.jsx
+++ src/components/Genres.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function Genres() {
+export default function Genres({ films, client }) {
+  const arr = [];
+  const [genresArr, setGenresArr] = useState(false);
+
+  useEffect(() => {
+    client
+      .getEntries({ content_type: 'genres' })
+      .then((response) => console.log('Genres von Contentful: ', response)) // =====> MUST BE ASYNC
+      .then((response) => setGenresArr(response));
+    sortFilms();
+  });
+
+  function sortFilms() {
+    if (genresArr) {
+      for (let i = 0; i < genresArr.length; i++) {
+        arr[i] = films.filter((el) => el.fields.genre === genresArr[i]);
+      }
+    }
+  }
+
+  console.log('Sortiert: ', arr);
+
   return <div>Genres</div>;
 }
