60b8cdcf305b2439573b15b817644e8b13e53b8f
Christian Löffler <cloeffle74@gmail.com>
2022-03-09 17:15:44 +0100

    implemented logic for Caroussel

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,63 @@
-import { Routes, Route, NavLink, useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import * as Contentful from "contentful";
-import Header from "./components/general/Header.jsx";
-import Footer from "./components/general/Footer.jsx";
-import "./assets/style.css";
+import { Routes, Route, NavLink, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import * as Contentful from 'contentful';
+import Header from './components/general/Header.jsx';
+import Footer from './components/general/Footer.jsx';
+import './assets/style.css';
+import Caroussel from './components/Caroussel.jsx';
 
 function App() {
+  const [films, setFilms] = useState(false);
+  const [randomFilms, setRandomFilms] = useState([]);
+
+  const client = Contentful.createClient({
+    space: '5o4kejg5nlut',
+    accessToken: 'IPErBwAcWvsPYzYEBdLbsMibGKstWOFf7yPBwZHWMSo',
+    host: 'cdn.contentful.com',
+  });
+
+  /* useEffect(async () => {
+    let fetching = await client.getEntries();
+    setFilms(fetching.items);
+  }, []); */
+  //
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    let fetching = await client.getEntries();
+    setFilms(fetching.items);
+  }
+
+  useEffect(() => {
+    if (films) {
+      let randomNumber = Math.floor(Math.random() * films.length);
+      let tempArr = [];
+      if (randomNumber == 0) {
+        for (let i = 0; i < 3; i++) {
+          tempArr.push(films[randomNumber + i]);
+        }
+      } else if (randomNumber == films.length) {
+        for (let i = 0; i < 3; i++) {
+          tempArr.push(films[randomNumber - i]);
+        }
+      } else {
+        tempArr.push(films[randomNumber]);
+        tempArr.push(films[randomNumber + 1]);
+        tempArr.push(films[randomNumber - 1]);
+      }
+
+      setRandomFilms(tempArr);
+    }
+  }, [films]);
+
   return (
     <div>
       <Header />
-      <div>
-        <p>Hier kommt content rein</p>
-      </div>
+      <Caroussel randomFilms={randomFilms} />
+      <div>{films && console.log(films)}</div>
       <Footer />
     </div>
   );
---
+++ src/components/Caroussel.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Caroussel({ randomFilms }) {
+  return <div>Caroussel</div>;
+}
+
+export default Caroussel;
