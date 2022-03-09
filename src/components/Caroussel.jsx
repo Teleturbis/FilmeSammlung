e2df4fa457cc7fdfa38520b30516595397ec2a05
Patrick Pluequett <ppluequett@gmail.com>
2022-03-09 17:49:50 +0100

    first draft

--- src/components/Caroussel.jsx
+++ src/components/Caroussel.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Caroussel({ randomFilms }) {
-  return <div>Caroussel</div>;
+  if (randomFilms) {
+    console.log(randomFilms[0]);
+  }
+  // console.log(randomFilms[0].fields.title);
+  // console.log(randomFilms[0].fields.previewImage.fields.file.url);
+
+  const [indexNumber, setIndexNumber] = useState(0);
+
+  const nextPicture = () => {
+    if (indexNumber == 2) {
+      setIndexNumber(0);
+    } else {
+      setIndexNumber(indexNumber + 1);
+    }
+  };
+
+  const previousPicture = () => {
+    if (indexNumber == 0) {
+      setIndexNumber(2);
+    } else {
+      setIndexNumber(indexNumber - 1);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={() => previousPicture()}>Left</button>
+      {randomFilms.length && (
+        <img
+          src={randomFilms[indexNumber].fields.previewImage.fields.file.url}
+          alt=""
+          className="animation"
+        />
+      )}
+      <button onClick={() => nextPicture()}>Right</button>
+    </div>
+  );
 }
 
 export default Caroussel;
