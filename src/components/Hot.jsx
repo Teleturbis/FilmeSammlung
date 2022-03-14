381ff6a36f1a09f879ad40e73975a44ee8442559
Patrick Pluequett <ppluequett@gmail.com>
2022-03-14 16:54:27 +0100

    add id and grid

--- src/components/Hot.jsx
+++ src/components/Hot.jsx
@@ -1,20 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import { DateTime } from 'luxon';
+import { NavLink } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
 function Hot({ films }) {
   const [hotFilms, setHotFilms] = useState([]);
-  console.log(hotFilms);
 
   useEffect(() => {
-    if (films) {
+    if (films.length > 0) {
       setHotFilms(films.filter((film) => film.fields.voting > 1000));
     }
   }, [films]);
 
   return (
-    <div>
-      <ul>
-        {hotFilms && hotFilms.map((film) => <li>{film.fields.title}</li>)}
-      </ul>
+    <div className="hotFilmsContainer">
+      {hotFilms.length > 0 &&
+        hotFilms.map((film) => (
+          <div key={uuidv4()} className="hotCard">
+            <h4 className="hotTitle">{film.fields.title}</h4>
+            <NavLink to="">
+              <img
+                className="hotPreviewImage"
+                src={film.fields.previewImage}
+                alt="previewImage"
+              />
+            </NavLink>
+          </div>
+        ))}
     </div>
   );
 }
