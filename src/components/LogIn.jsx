563da2ed0ba26871f7ec8cec0aefa32417198151
Kevin Poppe <kevin.poppe93@gmail.com>
2022-03-14 10:57:51 +0100

    added logIN

---
+++ src/components/LogIn.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import Modal from "./Modal.jsx";
+
+export default function LogIn({ client, userLoggedIn }) {
+  const [usernameInput, setUsernameInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+  const [modal, setModal] = useState(false);
+
+  function handleLogIn() {
+    client
+      .getEntries({
+        content_type: "user",
+        "fields.username": usernameInput,
+      })
+      .then((response) => {
+        if (
+          response.items[0] &&
+          response.items[0].fields.password === passwordInput
+        ) {
+          userLoggedIn(usernameInput);
+        } else if (response.items[0]) {
+          window.alert("FALSCHES PASSWORT");
+        }
+      })
+      .catch(window.alert("User nicht gefunden"));
+      setPasswordInput("")
+      setUsernameInput("")
+  }
+
+  function handleRegistration() {
+    setModal(true);
+  }
+
+  function changeModalDisplayed() {
+    setModal(!modal);
+  }
+
+  return (
+    <div>
+      {modal ? (
+        <Modal client={client} changeModalDisplayed={changeModalDisplayed} />
+      ) : null}
+      <div>
+        <input
+          type="text"
+          placeholder="Username"
+          value={usernameInput}
+          onChange={(e) => setUsernameInput(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Passwort"
+          value={passwordInput}
+          onChange={(e) => setPasswordInput(e.target.value)}
+        />
+        <input type="button" value="LogIn" onClick={() => handleLogIn()} />
+      </div>
+      <input
+        type="button"
+        value="Registrieren"
+        onClick={() => handleRegistration()}
+      />
+    </div>
+  );
+}
