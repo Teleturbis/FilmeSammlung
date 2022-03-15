5c3b8bd5f7373fa09bb7485809b1366efef72dfb
Kevin Poppe <kevin.poppe93@gmail.com>
2022-03-15 08:12:57 +0100

    deleted unnecessary console.log´s

--- src/components/GenreLi.jsx
+++ src/components/GenreLi.jsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import { Routes, Route, NavLink, useNavigate } from 'react-router-dom';
+import React from "react";
+import { Routes, Route, NavLink, useNavigate } from "react-router-dom";
 
 export default function GenreLi({ genre, sortedFilms, indexGenres }) {
-  console.log('LI', sortedFilms);
-
   if (sortedFilms.length === 0) return <></>;
 
   return (
     <div
       style={{
-        border: '1px solid rgb(200,200,200)',
-        margin: '3rem 5rem',
-        padding: '2rem 0',
-        borderRadius: '0.5rem',
+        border: "1px solid rgb(200,200,200)",
+        margin: "3rem 5rem",
+        padding: "2rem 0",
+        borderRadius: "0.5rem",
       }}
     >
       <h3
         style={{
-          textAlign: 'center',
-          fontSize: '5rem',
+          textAlign: "center",
+          fontSize: "5rem",
         }}
       >
         {genre}
       </h3>
-      <div style={{ display: 'flex', overflow: 'auto' }}>
+      <div style={{ display: "flex", overflow: "auto" }}>
         {sortedFilms.map((film, index) => (
           <NavLink
             style={{ backgroundImage: `url(${film.fields.previewImage})` }}
@@ -33,12 +31,12 @@ export default function GenreLi({ genre, sortedFilms, indexGenres }) {
           >
             <h4
               style={{
-                marginTop: '75%',
+                marginTop: "75%",
                 background:
-                  'radial-gradient(rgba(0,0,0,0.85) 45%, transparent 75% )',
-                padding: '3rem 4rem',
-                textAlign: 'center',
-                color: 'gray',
+                  "radial-gradient(rgba(0,0,0,0.85) 45%, transparent 75% )",
+                padding: "3rem 4rem",
+                textAlign: "center",
+                color: "gray",
               }}
             >
               {film.fields.title}
--- src/components/Genres.jsx
+++ src/components/Genres.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import GenreLi from './GenreLi.jsx';
+import React, { useState, useEffect } from "react";
+import GenreLi from "./GenreLi.jsx";
 
 export default function Genres({ films, client }) {
   const [sortedFilms, setSortedFilms] = useState(false);
@@ -7,7 +7,7 @@ export default function Genres({ films, client }) {
   const [genres, setGenres] = useState(false);
 
   useEffect(async () => {
-    let fetching = await client.getEntries({ content_type: 'genres' });
+    let fetching = await client.getEntries({ content_type: "genres" });
     setGenresArr(fetching.items);
   }, []);
 
@@ -18,10 +18,7 @@ export default function Genres({ films, client }) {
     }
   }, [genresArr]);
 
-  useEffect(() => console.log('films: ', films), [films]);
-
   useEffect(() => {
-    console.log('vor Sortieren', genres);
     if (films) {
       let arr = [];
       for (let i = 0; i < genres.length; i++) {
@@ -30,8 +27,6 @@ export default function Genres({ films, client }) {
       setSortedFilms(arr);
     }
   }, [films, genres]);
-
-  useEffect(() => console.log('Sortiert: ', sortedFilms), [sortedFilms]);
 
   return (
     <div>
